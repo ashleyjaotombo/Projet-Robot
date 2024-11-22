5d30a817ef591d1fbc2e7a00094758fc09ba0b34
ashleyjaotombo <harinambinina.jaotombo-ashley@efrei.net>
2024-11-22 21:13:14 +0100

    Update tree.c

--- tree.c
+++ tree.c
@@ -4,7 +4,6 @@
 #include "loc.h"
 #include "moves.h"
 #include "map.h"
-#include "stack.h"
 
 t_move* nouveauTableau(t_move* moves, int taille, int indice) {
     // On va allouer un tableau pour les mouvements restants
@@ -37,31 +36,30 @@ t_tree* creer_tree(int val, int nbreEnfts, t_move* moves, t_localisation localis
         node->enfants[i] = (t_node*)malloc(sizeof(t_node));
         node->enfants[i]->value = 0;        // Valeur par défaut
         node->enfants[i]->enfants = NULL;   // Aucun sous-enfant
-        node->enfants[i]->nb_enfants = 0;
+        node->enfants[i]->nb_enfants =0;
         node->enfants[i]->parent=node;
     }
     node->nb_enfants=nbreEnfts;
+    node->mouvement=B_10;
     tree->root=node;
     return tree;
 }
 
 
 void completerTreeRec(t_node* node, int n, t_map map) {
-  t_localisation nouvelle_loc;
+  t_localisation nouvelle_loc=node->localisation;
   t_localisation loc=node->localisation;
   t_move* tableau=node->mouvementsRestants;
 
   //CONTRAINTE SUR LES SOLS REG
   if (isValidLocalisation(loc.pos, map.x_max, map.y_max)){
     if (map.soils[loc.pos.x][loc.pos.y] == REG && node->depth==0)
-      node->depth++;
+      node->depth=1;
   }
 
   // On complète les enfants par niveau
   for (int i = 0; i < n; i++) {
-    nouvelle_loc = move(loc, tableau[i]); // On calcule la nouvelle localisation après le mouvement
-
-    //CONTRAINTE SUR LES SOLS ERG
+      //CONTRAINTE SUR LES SOLS ERG
     if (map.soils[nouvelle_loc.pos.x][nouvelle_loc.pos.y] == ERG) {
         if (tableau[i]==F_20)
           nouvelle_loc = move(loc, F_10);
@@ -77,9 +75,10 @@ void completerTreeRec(t_node* node, int n, t_map map) {
 
     // Vérification de la validité de la nouvelle position
   	if (isValidLocalisation(nouvelle_loc.pos, map.x_max, map.y_max) && node->value<=500) {
-          node->enfants[i]->value = map.costs[nouvelle_loc.pos.y][nouvelle_loc.pos.x]; // On affecte la valeur à l'enfant
+          node->enfants[i]->value = map.costs[nouvelle_loc.pos.x][nouvelle_loc.pos.y]; // On affecte la valeur à l'enfant
     }
-    else {
+    else
+    {
       node->enfants[i]->value = 65000; // On affecte une valeur élevée si la position est hors-map
     }
 
@@ -95,10 +94,28 @@ void completerTreeRec(t_node* node, int n, t_map map) {
 
 	// On va jusqu'à 3 de profondeur car on fait 3 mouvements
     if (node->enfants[i]->depth == 2) {
+        node->enfants[i]->nb_enfants=0;
         return; // la condition d'arrêt de la récursion
     }
 
     // On initialise les enfants (qui ne sont pas encore créés) des noeuds
     if (node->enfants[i]->enfants == NULL) {
       node->enfants[i]->enfants = (t_node**)malloc((n-1) * sizeof(t_node*));
-      for (int j 
+      for (int j = 0; j < n-1; j++) {
+        node->enfants[i]->enfants[j] = (t_node*)malloc(sizeof(t_node));
+        node->enfants[i]->enfants[j]->value = 0;        // On donne 0 comme valeur par défaut
+        node->enfants[i]->enfants[j]->enfants = NULL;   // On ne leur donne aucun enfant
+        node->enfants[i]->enfants[j]->nb_enfants = 0;
+        node->enfants[i]->enfants[j]->parent=node->enfants[i];
+      }
+    }
+	// On fait un appel récursif pour les sous-enfants
+	completerTreeRec(node->enfants[i], n-1, map);
+  }
+}
+
+void completerParcours(t_tree* tree, int n, t_map map)
+{
+    completerTreeRec(tree->root, n, map);
+}
+
